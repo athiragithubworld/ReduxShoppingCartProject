a36c1b45bc071366796aa1140bd040b503b23687
athiragithubworld <athirahrms@gmail.com>
2023-07-25 15:09:26 +0530

    Products  js (task - 19) dummy products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,41 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const dummyProducts = [
+  {
+    id: "p1",
+    price: 6,
+    title: "Story Book",
+    description: "Children Story Book",
+  },
+  {
+    id: "p2",
+    price: 5,
+    title: "Novel",
+    description: "Book for adult",
+  },
+  {
+    id: "p3",
+    price: 9,
+    title: "Auto Biography",
+    description: "Insipirational Story",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyProducts.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
