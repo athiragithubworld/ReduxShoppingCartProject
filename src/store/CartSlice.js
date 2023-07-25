527bf67026f671669ee5babee1217539ad850698
athiragithubworld <athirahrms@gmail.com>
2023-07-25 15:05:13 +0530

    CartSlice js (Task - 19) Add , remove item

--- src/store/CartSlice.js
+++ src/store/CartSlice.js
@@ -1,13 +1,54 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { showCart: true };
+const initialState = { items: [], totalAmount: 0 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    cartHandler(state) {
-      state.showCart = !state.showCart;
+    addToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+
+      state.totalAmount++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      state.totalAmount--;
+
+      // state.changed = true;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+      // state.totalAmount = state.totalAmount - newItem.price;
+      // state.items.forEach((item) => {
+      //   if (item.id === newItem.id && newItem.quantity <= 1) {
+      //     item.quantity = Number(item.quantity) - Number(newItem.quantity);
+
+      //     if (item.quantity === 0) {
+      //       state.items = state.items.filter((pdt) => pdt.id !== item.id);
+      //     }
+      //   }
+      //   if (item.id === newItem.id && newItem.quantity > 1) {
+      //     item.quantity = Number(item.quantity) - 1;
+      //   }
+      // });
     },
   },
 });
