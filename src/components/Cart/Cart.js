19da0216a036f7419b7efaefb38b682688f5559b
athiragithubworld <athirahrms@gmail.com>
2023-07-25 15:05:56 +0530

    Cart js (Task -19) show items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,37 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartitems = useSelector((state) => state.cart.items);
+  console.log("cart", cartitems);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {/* <CartItem
+          item={{
+            id,
+            title: "Athi",
+            quantity: 1,
+            total: 20,
+            price: 20,
+          }}
+        /> */}
+        {cartitems.map((itm) => (
+          <CartItem
+            key={itm.id}
+            item={{
+              id: itm.id,
+              title: itm.name,
+              quantity: itm.quantity,
+              total: itm.totalPrice,
+              price: itm.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
